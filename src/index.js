4b3d45200f0335bcacace6f603141826241a01a2
jeriny <monicayerin40@gmail.com>
2024-08-22 22:44:02 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import ReactDOM from 'react-dom';
-import App from './pages/App';
+import { createRoot } from 'react-dom/client'; 
+import Main from './main';
+import Modal from 'react-modal';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Modal.setAppElement('#root'); 
+
+const container = document.getElementById('root'); 
+const root = createRoot(container); 
+root.render(<Main />); 
